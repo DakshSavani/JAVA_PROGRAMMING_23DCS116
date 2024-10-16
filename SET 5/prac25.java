679facbaed3cbc721967ea80190cf5242c6d8b24
DakshSavani <23DCS116@charusat.edu.in>
2024-10-16 17:02:05 +0530

    java
    java

---
+++ SET 5/prac25.java
@@ -0,0 +1,12 @@
+public class prac25 {
+    public static void main(String[] args) {
+    try {
+    throw new Exception("This is a custom exception");
+    } catch (Exception e) {
+    System.out.println("Caught an exception: " + e.getMessage());
+    }
+    System.out.print("\nName : DAKSH SAVANI_23DCS116 ");
+    
+    }
+    }
+    
